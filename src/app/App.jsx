fb0471c8def1c93c65f1c2b77327f060fbf01b5d
illia <fialkivskiji@gmail.com>
2024-04-21 20:27:36 +0300

    tailwind

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 
-function App() {
+export default function App() {
   return (
-    <p>hello</p>
-  );
+    <h1 className="text-3xl font-bold underline">
+      Hello world!
+    </h1>
+  )
 }
-
-export default App;
--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './app/App.jsx'
-import './app/index.css'
+import './main.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
