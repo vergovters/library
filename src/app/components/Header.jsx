e3de2ac0375df01ce62df151fc06eedca2aee61c
illia <fialkivskiji@gmail.com>
2024-04-23 00:22:50 +0300

    user ready

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../context/AuthContext';
 
 const Header = () => {
+    const { user, dispatch } = useContext(AuthContext);
+
+
+    const logout = async (e) => {
+        e.preventDefault();
+        try {
+        dispatch({ type: "LOGOUT" });
+        } catch (err) {
+        dispatch({ type: "LOGOUT", payload: err.response.data });
+    }
+  };
+
+  console.log(user)
     return (
         <header class="h-24 sm:h-32 flex items-center z-30 w-full dark:bg-gray-800 bg-white">
                 <div class="container mx-auto px-6 flex items-center justify-between">
@@ -19,9 +33,17 @@ const Header = () => {
                             <Link to="/authors" className="py-2 px-6 flex transform hover:translate-y-[-3px] transition-all duration-300">
                                 Authors
                             </Link>
-                            <Link to="/registration" className="py-2 px-6 flex transform hover:translate-y-[-3px] transition-all duration-300">
-                                Register
-                            </Link>
+                            {user ? (
+                                <div className='flex text-lg'>
+                                    <p className='py-2 px-6 text-pink-500'>{user.name}</p>
+                                    <button className="flex transform uppercase hover:translate-y-[-3px] transition-all py-2 duration-300 text-lg" onClick={logout}>Logout</button>
+                                </div>
+                            ): (
+                                <Link to="/registration" className="py-2 px-6 flex transform hover:translate-y-[-3px] transition-all duration-300">
+                                    Register
+                                </Link>
+                            )}
+                            
                         </nav>
                         <button class="lg:hidden flex flex-col ml-4">
                             <span class="w-6 h-1 bg-gray-800 dark:bg-white mb-1">
