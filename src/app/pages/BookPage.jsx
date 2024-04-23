24acea9c202f1af323d01c1f0828e1f16d88d58b
illia <fialkivskiji@gmail.com>
2024-04-23 23:11:44 +0300

    text

--- src/app/pages/BookPage.jsx
+++ src/app/pages/BookPage.jsx
@@ -1,10 +1,77 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import { useParams } from 'react-router-dom';
+import axios from 'axios';
+const url = import.meta.env.VITE_BACKEND_URL;
 
 const BookPage = () => {
+    const {id} = useParams()
+    const [page, setPage] = useState(1);
+    const [text, setText] = useState("");
+    const [title, setTitle] = useState("");
+    const [totalPages, setTotalPages] = useState(0);
+    const chars = 4000; 
+
+    const handleNextPage = () => {
+        setPage(prevPage => Math.min(prevPage + 1, totalPages));
+    }
+
+    const handlePrevPage = () => {
+        setPage(prevPage => Math.max(prevPage - 1, 1));
+    }
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${url}/api/writings/${id}?page=${page}&size=${chars}`);
+                setText(response.data);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
+
+        fetchData();
+    }, [page]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${url}/api/writings/info/${id}`);
+                setTitle(response.data);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
+
+        fetchData();
+
+        const fetchPages = async () => {
+            try {
+                const response = await axios.get(`${url}/api/writings/total-pages/${id}?size=${chars}`);
+                setTotalPages(response.data);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
+
+        fetchPages();
+    }, []);
+
+    console.log(title)
+
     return (
         <div>
-            book
-        </div>
+            <p>{title.name}</p>
+            <p className='w-[900px]'>{text}</p>
+            <div className='flex items-center'>
+                <button className='mr-2' onClick={handlePrevPage} disabled={page === 1}>
+                    Prev
+                </button>
+                <span>{page} of {totalPages}</span>
+                <button className='ml-2' onClick={handleNextPage} disabled={page === totalPages}>
+                    Next
+                </button>
+            </div>
+        </div> 
     );
 }
 
--- src/app/pages/BooksPage.jsx
+++ src/app/pages/BooksPage.jsx
@@ -1,13 +1,56 @@
-import React, {useContext} from 'react';
-import { AuthContext } from '../context/AuthContext';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import BookCard from '../components/BookCard';
+
+const url = import.meta.env.VITE_BACKEND_URL;
+const ITEMS_PER_PAGE = 10; 
 
 const BooksPage = () => {
-    const { user ,loading, error, dispatch } = useContext(AuthContext);
+    const [books, setBooks] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(`${url}/api/writings`);
+                setBooks(response.data);
+            } catch (error) {
+                console.error('Error fetching books:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
+
+    const totalPages = Math.ceil(books.length / ITEMS_PER_PAGE);
+    const indexOfLastBook = currentPage * ITEMS_PER_PAGE;
+    const indexOfFirstBook = indexOfLastBook - ITEMS_PER_PAGE;
+    const currentBooks = books.slice(indexOfFirstBook, indexOfLastBook);
+
+    const handleNextPage = () => {
+        setCurrentPage(prevPage => Math.min(prevPage + 1, totalPages));
+    };
+
+    const handlePrevPage = () => {
+        setCurrentPage(prevPage => Math.max(prevPage - 1, 1));
+    };
+
     return (
         <div>
-            bookspage
+            {currentBooks.map((book) => {
+                return <BookCard key={book.id} book={book} />;
+            })}
+            <div className='flex items-center'>
+                <button className='mr-2' onClick={handlePrevPage} disabled={currentPage === 1}>
+                    Prev
+                </button>
+                <span>{currentPage} of {totalPages}</span>
+                <button className='ml-2' onClick={handleNextPage} disabled={currentPage === totalPages}>
+                    Next
+                </button>
+            </div>
         </div>
     );
-}
+};
 
 export default BooksPage;
